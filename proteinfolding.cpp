c3df2fa46142b1c009794761625b9f86c7b33bbf
sebastian ojeda <sojedaf@unal.edu.co>
2022-04-19 12:28:51 -0500

    definicion matriz J

--- proteinfolding.cpp
+++ proteinfolding.cpp
@@ -3,6 +3,8 @@
 #include <eigen3/Eigen/Dense>
 #include <cstdlib>
 #include <random>
+#include <vector>
+using namespace std;
 
 double move(double x, int N);
 double energy(double x, int N); // declaration
@@ -12,71 +14,50 @@ int main(int argc, char **argv)
     std::cout.setf(std::ios::scientific);
     std::cout.precision(6);
     // declare variables
-    int n = atoi(argv[1]);
+    int N = atoi(argv[1]);
     //const double exact = (argv[2]);
-    //
-    //const int size = 10;
-    //matriz posiciones
-    //double * array;
-    //int* pos=new int [n][4];
+
 
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(1, 20);
+    std::uniform_int_distribution<> distrib(1, 20);//tipo de aminoacido
+
+    std::random_device rd1;  //W
+    std::mt19937 gen1(rd1()); //
+    std::uniform_real_distribution<> distribucion(-4,-2);//valores para la matriz J
+
+
+    vector<vector<int> > vec{};
+    double matrizj[20][20];
+
+
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++){
+            matrizj[i][j]=distribucion(gen1);
+            std::cout << matrizj[i][j] << " ";}
+        std::cout << std::endl;
+     }
 
 
 
-
-
-    //A algo aleatorio
-
-
-    int pos[n][4];
-    //for(int i = 0; i < n; i++){
-     //   for(int j = 0; j < 4; i++){
-      //      if (j==3){
-       //         pos[i][j]=i;
-        //    }
-         //   if (j==4){
-         //       pos[i][j]=A;
-          //  }
-           // pos[i][j]={}
-
-
-
-
-
-
-
-
-    for(int i = 0; i < n; i++){
-        pos[i][0]=i;
-        pos[i][1]=0;
-        pos[i][2]=i;
-        //pos[i][3]=std::rand(20);
-        pos[i][3]=distrib(gen);
-        std::cout<< pos[i] <<"\t";
-
+    for(int i = 0; i < N; i++){
+        std::vector<int> amino ={i,0,i,distrib(gen)};
+        vec.push_back(amino);
 
     }
+     for (int i = 0; i < vec.size(); i++) {
+         for (int j = 0; j < vec[i].size(); j++){
+            std::cout << vec[i][j] << " ";}
+        std::cout << std::endl;
+     }
 
-
-
-
-
-
-    //reserve memory
-
-    std::cout<< pos;
+    //std::cout<< pos;
     //array = new double [N];
     //delete [] array;
     //delete [] pos;
     //HAGA ALGO CON LAS FUNCIONES
     //move(double x, int N);
     //energy(double x, int N);
-
-
-
      //std::cout << a << std::endl; PARA MOSTAR ALGO EN PANTALLA
 
      return 0;
