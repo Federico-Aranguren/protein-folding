95ebe21041d4fa5f327f24ea45c0ac3e5e27c8c3
Federico-Aranguren <faranguren@unal.edu.co>
2022-04-21 22:05:53 -0500

    check posible positions was added, preserve long of the chain, names of var. were changed and comments added

--- proteinfolding.cpp
+++ proteinfolding.cpp
@@ -4,9 +4,10 @@
 #include <cstdlib>
 #include <random>
 #include <vector>
+#include <math.h>
 using namespace std;
 
-double move(double x, int N);
+double move(int a, std::vector<vector<int>> b);
 double energy(double x, int N); // declaration
 
 int main(int argc, char **argv)
@@ -15,41 +16,48 @@ int main(int argc, char **argv)
     std::cout.precision(6);
     // declare variables
     int N = atoi(argv[1]);
-    //const double exact = (argv[2]);
+    int n = 20;
+    //const int steps = (argv[2]);
+    vector<vector<int> > Protein = {{0,0},{1,0},{1,1}};
+    double matrizj[n][n];
 
+    
+    std::random_device rd;  
+    std::mt19937 genn(rd()); 
+    std::uniform_int_distribution<> distribn(1, n);//Ran number for amino type
 
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(1, 20);//tipo de aminoacido
+    std::random_device rd1;  
+    std::mt19937 genJ(rd1()); 
+    std::uniform_real_distribution<> distribJ(-4,-2);//Ran number for matrix J
 
-    std::random_device rd1;  //W
-    std::mt19937 gen1(rd1()); //
-    std::uniform_real_distribution<> distribucion(-4,-2);//valores para la matriz J
-
-
-    vector<vector<int> > vec{};
-    double matrizj[20][20];
-
-
-    for (int i = 0; i < 20; i++) {
-        for (int j = 0; j < 20; j++){
-            matrizj[i][j]=distribucion(gen1);
-            std::cout << matrizj[i][j] << " ";}
+    std::random_device rd2;
+    std::mt19937 genN(rd2());
+    std::uniform_int_distribution<> distribN(1, N);// Ran number for the chain
+    
+    int randomposition = distribN(genN);
+    
+    for (int i = 0; i < n; i++) { // Matrix construction
+        for (int j = 0; j < n; j++){
+            matrizj[i][j]=distribJ(genJ);
+            //std::cout << matrizj[i][j] << " ";
+            }
         std::cout << std::endl;
      }
 
-
-
-    for(int i = 0; i < N; i++){
-        std::vector<int> amino ={i,0,i,distrib(gen)};
-        vec.push_back(amino);
+    for(int i = 0; i < N; i++){ //Amino vector construction
+        std::vector<int> amino ={i,0,i,distribn(genn)};
+        //Protein.push_back(amino); //Protein construction
 
     }
-     for (int i = 0; i < vec.size(); i++) {
-         for (int j = 0; j < vec[i].size(); j++){
-            std::cout << vec[i][j] << " ";}
+     for (int i = 0; i < Protein.size(); i++) { //Protein construction
+         for (int j = 0; j < Protein[i].size(); j++){
+            //std::cout << Protein[i][j] << " ";
+            }
         std::cout << std::endl;
      }
+
+    cout << randomposition << "\n";
+    move(randomposition, Protein);
 
     //std::cout<< pos;
     //array = new double [N];
@@ -63,9 +71,44 @@ int main(int argc, char **argv)
      return 0;
 }
 
-double move(double x, int N) // implementation
+double move(int a, std::vector<vector<int>> b) // implementation
 {
+    
+    std::vector<vector<int> > posible_movements{{b[a][0] + 1,b[a][1] + 1},{b[a][0] + 1,b[a][1] -1},{b[a][0] - 1,b[a][1] + 1},{b[a][0] - 1,b[a][1] - 1}};
 
+    for (int j = 0; j < b.size(); j++){
+        
+        if(b[j][0] == b[a][0] + 1 and b[j][1] == b[a][1] + 1){
+            posible_movements.erase(posible_movements.begin());
+        }
+        else if (b[j][0] == b[a][0]+1 and b[j][1] == b[a][1] - 1)
+        {
+            posible_movements.erase(posible_movements.begin()+1);
+        }
+        else if (b[j][0] == b[a][0] - 1 and b[j][1] == b[a][1] + 1)
+        {
+            //posible_movements.erase(posible_movements.begin()+2);
+        }
+        else if (b[j][0] == b[a][0]-1 and b[j][1] == b[a][1] - 1)
+        {
+            posible_movements.erase(posible_movements.begin()+3);
+        }
+
+    }
+    for (int i = 0; i < posible_movements.size()+int(1); i++)
+    {
+        float d1 = sqrt(std::pow(posible_movements[i][0]-b[a+1][0],2)+std::pow(posible_movements[i][1]-b[a+1][1],2));
+        float d2 = sqrt(std::pow(posible_movements[i][0]-b[a-1][0],2)+std::pow(posible_movements[i][1]-b[a-1][1],2));
+        cout << d1 << "\t" << d2 << "\n"<< posible_movements[i][0] << "\t" << posible_movements[i][1] << "\n";
+        if (d1 != 1 || d2 != 1){
+            posible_movements.erase(posible_movements.begin()+i);
+        }
+        
+
+    }
+    
+
+ 
     return 0;
 }
 double energy(double x, int N) // implementation
