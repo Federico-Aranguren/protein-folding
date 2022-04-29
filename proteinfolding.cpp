436d1e0d4f47ea5289c978c2c2f147e83c557a7f
Federico-Aranguren <faranguren@unal.edu.co>
2022-04-28 22:24:05 -0500

    Now the protein moves :D. I've changed the mov fun for posible_mov function and ordered it to return me a vector that selects a random posible movement for the amino, then added the energy condition and set the probability for the amino to fold. I'm happy :)

--- proteinfolding.cpp
+++ proteinfolding.cpp
@@ -7,7 +7,7 @@
 #include <math.h>
 using namespace std;
 
-double move(int a, std::vector<vector<int>> b);
+vector<int> posible_move(int a, std::vector<vector<int>> b);
 double energy(std::vector<vector<int>> P, double Matriz[8][8]); // declaration
 
 int main(int argc, char **argv)
@@ -18,35 +18,37 @@ int main(int argc, char **argv)
     int N = atoi(argv[1]);
     int n = 8;
     int T = 1; //Temperatura del medio
-    //const int steps = (atoi[2]);
+    const int steps = atoi(argv[2]);
     vector<vector<int> > Protein = {{0,0,1},{1,0,2},{1,1,3},{0,1,4},{-1,1,5},{-1,0,6},{-1,-1,7},{0,-1,8}};
     double matrizj[8][8];
-    
+    double deltaE;
     
     std::random_device rd;  
     std::mt19937 genn(rd()); 
-    std::uniform_int_distribution<> distribn(1, n);//Ran number for amino type
+    std::uniform_int_distribution<> distribn(1, n);//Ran int number for amino type
 
     std::random_device rd1;  
     std::mt19937 genJ(rd1()); 
-    std::uniform_real_distribution<> distribJ(-4,-2);//Ran number for matrix J
+    std::uniform_real_distribution<> distribJ(-4,-2);//Ran real number for matrix J
 
     std::random_device rd2;
     std::mt19937 genN(rd2());
-    std::uniform_int_distribution<> distribN(0, N-1);// Ran number for the chain
-    
-    int randomposition = distribN(genN);
+    std::uniform_int_distribution<> distribN(0, N-1);// Ran int number for the chain
+
+    std::random_device rd4;
+    std::mt19937 genP(rd4());
+    std::uniform_real_distribution<> distribP(0, 1);// Ran real number for set the probability of movement
     
     for (int i = 0; i < n; i++) { // Matrix construction
-      cout <<"------------------------------------------------------------------------------------------------------------------" << "\n";
+    //   cout <<"------------------------------------------------------------------------------------------------------------------" << "\n";
         for (int j = i; j < n; j++){
             matrizj[i][j]=distribJ(genJ);
 	        matrizj[j][i]=matrizj[i][j];
         }
-	    for (int l = 0; l < n; l++){
-	        std::cout << matrizj[i][l] << "\t";
-	    }
-        std::cout << "\n";
+	    // for (int l = 0; l < n; l++){
+	    //     std::cout << matrizj[i][l] << "\t";
+	    // }
+        // std::cout << "\n";
     }
 
     for(int i = 0; i < N; i++){ //Amino vector construction
@@ -59,27 +61,39 @@ int main(int argc, char **argv)
             //std::cout << Protein[i][j] << " ";    
             }
         std::cout << std::endl;
-     }
-    //cout << Protein.size() << "\n";
-    move(randomposition, Protein);
-    energy(Protein, matrizj);
-
-    //std::cout<< pos;
-    //array = new double [N];
-    //delete [] array;
-    //delete [] pos;
-    //HAGA ALGO CON LAS FUNCIONES
-    //move(double x, int N);
-    //energy(double x, int N);
-     //std::cout << a << std::endl; PARA MOSTAR ALGO EN PANTALLA
-
-     return 0;
+    }
+    
+    for (int ms = 0; ms <= steps; ms++){
+        int randomposition = distribN(genN);
+        vector<vector<int>> aux_vec = Protein;
+        aux_vec[randomposition] = posible_move(randomposition, Protein);
+        deltaE = energy(aux_vec, matrizj) - energy(Protein, matrizj);
+        if (deltaE <= 0){
+            Protein = aux_vec;
+        }
+        else{
+            double Boltz_factor = exp(-deltaE/T);
+            float prob = distribP(genP);
+            if (prob >= Boltz_factor){
+                Protein = aux_vec;
+            }
+        }
+        cout << "\nProteína: \t";
+        for (int i=0; i < Protein.size();i++){
+            cout << "(";
+            for (int j=0; j < Protein[i].size();j++){
+                cout << Protein[i][j]<<",";
+            }
+            cout << "),\t";
+        }
+    }
+    return 0;
 }
 
-double move(int a, std::vector<vector<int>> b) // implementation
+vector<int> posible_move(int a, std::vector<vector<int>> b) // implementation
 {
     
-    std::vector<vector<int> > posible_movements{{b[a][0] + 1,b[a][1] + 1},{b[a][0] + 1,b[a][1] -1},{b[a][0] - 1,b[a][1] + 1},{b[a][0] - 1,b[a][1] - 1}};
+    std::vector<vector<int> > posible_movements{{b[a][0] + 1,b[a][1] + 1,b[a][2]},{b[a][0] + 1,b[a][1] -1,b[a][2]},{b[a][0] - 1,b[a][1] + 1,b[a][2]},{b[a][0] - 1,b[a][1] - 1,b[a][2]}};
     // cout << a << "\n";
     // cout << b[a][0] << "\t" <<b[a][1] << "\n";
     for (int j = 0; j < b.size(); j++){
@@ -102,10 +116,7 @@ double move(int a, std::vector<vector<int>> b) // implementation
         }    
     }
     //cout << "Sin tener en cuenta la longitud" << "\n"<< a << "\n" << b[a][0] << "\t"<< b[a][1]<< "\n";
-    //for (int i = 0; i < posible_movements.size(); i++){
-    //    for (int j = 0; j < posible_movements[i].size(); j++){
-    //        cout << posible_movements[i][j] << "\t";
-    //    }
+    //for (int i = 0; i < posible_movements.size(); i++){<< "\t"
     //    cout << "\n";
     //}
     // cout << "----------" << "\n";
@@ -139,23 +150,34 @@ double move(int a, std::vector<vector<int>> b) // implementation
             k = 0;
         }
     }
-
-
-    // cout << "Teniendo en cuenta la longitud:" << "\n";
-    // for (int i = 0; i < posible_movements.size(); i++){
-    //    for (int j = 0; j < posible_movements[i].size(); j++){
-    //         cout << posible_movements[i][j] << "\t";
-    //     }
-    //     cout << "\n";
-    // }
-    
-    return 0;
+    cout << "amino choosen:\t"<< a << "\n";
+    if (!posible_movements.empty()){
+        std::random_device rd3;
+        std::mt19937 genpm(rd3());
+        std::uniform_int_distribution<> distribpm(0, posible_movements.size()-1);// Ran number for posible movements
+        int randommov = distribpm(genpm);
+        cout << "Posible movement:\t" << randommov << "\n" << "Posible_movements:" << "\t";
+        for (int i = 0; i < posible_movements.size(); i++){
+        for (int j = 0; j < posible_movements[i].size(); j++){
+            cout << posible_movements[i][j] << "\t";
+        }
+        cout << "\nvector posible_mov:\t";
+        }
+        for (int i = 0; i < posible_movements[randommov].size(); i++){
+            cout <<posible_movements[randommov][i] << "\t";
+        }
+        cout << "\n";
+        return posible_movements[randommov];
+    }
+    else{
+        return b[a];
+    }
 }
 double energy(std::vector<vector<int>> P,double Matriz[8][8]){// implementation
-  float E = 0;
+  double E = 0;
     for (int i = 0; i < P.size(); i++){//se situa en un aminoácido
-        cout << i << "\n" << P[i][0] << "\t" << P[i][1] << "\n";
-        cout << "---------------" << "\n";
+        //cout << i << "\n" << P[i][0] << "\t" << P[i][1] << "\n";
+        //cout << "---------------" << "\n";
         std::vector<vector<int> > closest_amino{{P[i][0] + 1,P[i][1],0},{P[i][0],P[i][1] + 1,0},{P[i][0] - 1,P[i][1], 0},{P[i][0],P[i][1]-1, 0}};
         for (int j = 0; j < closest_amino.size(); j++){//se situa en las posiciones cercanas del i.esimo aminoácido
             for (int l = 0; l < P.size(); l++){//Revisa si algun amino está en la posicion j-esima de closest_position
